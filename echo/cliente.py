ed1f2ba0bccca9ffad6d1ea640264c4dde062925
JavAlisson <javalisson@gmail.com>
2017-05-31 01:02:56 -0300

    Adicionados o host e a mensagem como parâmetros opcionais no cliente

--- echo/cliente.py
+++ echo/cliente.py
@@ -3,13 +3,19 @@
 
 # adaptado de https://wiki.python.org/moin/TcpCommunication
 
+import sys
 import socket
-
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = "Ola, Mundo!"
+
+if len(sys.argv) >= 2:
+    TCP_IP = sys.argv[1]
+
+if len(sys.argv) >= 3:
+    MESSAGE = sys.argv[2]
 
 print ("[CLIENTE] Iniciando")
 
