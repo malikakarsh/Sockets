d8e622d093d012007993c541852712f5635c8112
JavAlisson <javalisson@gmail.com>
2017-06-21 00:32:48 -0300

    Adicionados suporte a mensagens longas e a conexões consecutivas

--- echo/cliente.py
+++ echo/cliente.py
@@ -8,8 +8,8 @@ import socket
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 1024
-MESSAGE = "Ola, Mundo!"
+BUFFER_SIZE = 20
+MESSAGE = "Olá Mundo!"
 
 if len(sys.argv) >= 2:
     TCP_IP = sys.argv[1]
@@ -23,11 +23,21 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print ("[CLIENTE] Conectando")
 s.connect((TCP_IP, TCP_PORT))
 print ("[CLIENTE] Enviando dados: \"" + MESSAGE + "\"")
-s.send(MESSAGE.encode('utf-8'))
-print ("[CLIENTE] Recebendo dados do servidor")
-data = s.recv(BUFFER_SIZE)
-print ("[CLIENTE] Dados recebidos em resposta do servidor: \"" + data.decode('utf-8') + "\"")
-print ("[CLIENTE] Fechando conexão com o servidor")
+s.send((MESSAGE + '\n').encode('utf-8'))
+print ("[CLIENTE] Recebendo dados do CLIENTE")
+msg = ''
+fim_msg = False
+dados = bytearray()
+while not fim_msg:
+	recvd = s.recv(BUFFER_SIZE)
+	dados += recvd
+	print ("[CLIENTE] Recebidos ", len(recvd), " bytes")
+	if b'\n' in recvd:
+		msg = dados.rstrip(b'\n').decode('utf-8')
+		fim_msg = True
+print ("[CLIENTE] Recebidos no total ", len(dados), " bytes")
+print ("[CLIENTE] Dados recebidos em resposta do CLIENTE: \"" + msg + "\"")
+print ("[CLIENTE] Fechando conexão com o CLIENTE")
 s.close()
 
 print ("[CLIENTE] Fim")
--- echo/servidor.py
+++ echo/servidor.py
@@ -9,6 +9,34 @@ TCP_IP = ''
 TCP_PORT = 5005
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
+def atende_cliente(conn, addr):
+    while 1:
+        msg = ''
+        dados = bytearray()
+        print ("[SERVIDOR] Aguardando dados do cliente")
+        fim_msg = False
+        try:
+            while not fim_msg:
+                recvd = conn.recv(BUFFER_SIZE)
+                if not recvd:
+                    raise ConnectionError()
+                    break
+                dados += recvd
+                print ("[SERVIDOR] Recebidos ", len(recvd), " bytes")
+                if b'\n' in recvd:
+                    msg = dados.rstrip(b'\n').decode('utf-8')
+                    fim_msg = True
+            print ("[SERVIDOR] Recebidos no total ", len(dados), " bytes")
+            print ("[SERVIDOR] Dados recebidos do cliente com sucesso: \"" + msg + "\"")
+            print ("[SERVIDOR] Enviando resposta para o cliente")
+            conn.send(dados)  # echo
+            print ("[SERVIDOR] Resposta enviada: \"" + msg + "\"")
+        except BaseException as erro:
+            print ("[SERVIDOR] [ERROR] Socket error: ", erro)
+            break
+    print ("[SERVIDOR] Fechando a conexao ", addr)
+    conn.close()
+
 print ("[SERVIDOR] Iniciando")
 
 print ("[SERVIDOR] Abrindo a porta " + str(TCP_PORT) + " e ouvindo")
@@ -16,17 +44,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-print ("[SERVIDOR] Aguardando conexao")
-conn, addr = s.accept()
-print ('[SERVIDOR] Conexao com o cliente realizada. Endereco da conexao:', addr)
 while 1:
-    print ("[SERVIDOR] Aguardando dados do cliente")
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print ("[SERVIDOR] Dados recebidos do cliente com sucesso: \"" + data.decode('utf-8') + "\"")
-    print ("[SERVIDOR] Enviando resposta para o cliente")
-    conn.send(data)  # echo
-    print ("[SERVIDOR] Resposta enviada: \"" + data.decode('utf-8') + "\"")
+    print ("[SERVIDOR] Aguardando conexao")
+    conn, addr = s.accept()
+    print ('[SERVIDOR] Conexao com o cliente realizada. Endereco da conexao:', addr)
+    atende_cliente(conn, addr)
+
 print ("[SERVIDOR] Fechando a porta " + str(TCP_PORT))
-conn.close()
-print ("[SERVIDOR] Fim")
+s.close()
+
+print ("[SERVIDOR] fim_msg")
