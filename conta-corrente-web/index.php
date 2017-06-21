8446489c072a64d46182f4818ca341b9fb982bcc
JavAlisson <javalisson@gmail.com>
2017-06-21 02:00:01 -0300

    Adicionadas as operações possíveis à interface Web

--- conta-corrente-web/index.php
+++ conta-corrente-web/index.php
@@ -2,13 +2,18 @@
 <html>
 <head>
 	<title>Conta-Corrente</title>
+	<style>
+		form {
+			margin: 2em 0;
+		}
+	</style>
 </head>
 <body>
 <h1>Conta-Corrente</h1>
 <p>Bem-vindo!</p>
 <pre><?php
 $address = "127.0.0.1";
-$service_port = 6006;
+$service_port = 7007;
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 if ($socket === false) {
@@ -25,14 +30,37 @@ if ($result === false) {
 //     echo "OK.\n";
 }
 
-$in = "SALDO";
+$operacao = isset($_GET["operacao"]) && $_GET["operacao"] != "" ? $_GET["operacao"] : "SALDO";
+$in = $operacao;
+if ($operacao == "DEBITO" || $operacao == "CREDITO") {
+	$valor = $_GET["valor"];
+	$in .= " " . $valor;
+}
+$in .= "\n";
 socket_write($socket, $in, strlen($in));
 
 $out = socket_read($socket, 2048);
 
-echo "SALDO: " . $out;
+if ($operacao == "EXTRATO") {
+	echo "EXTRATO: " . $out;
+} else {
+	echo "SALDO: " . $out;	
+}
 
 socket_close($socket);
 
 ?></pre>
+<form action="." method="GET">
+<input autofocus onfocus="this.select()" type="number" name="valor" value="0" />
+<select name="operacao">
+	<option value="">Selecione</option>
+	<option value="CREDITO">Crédito</option>
+	<option value="DEBITO">Débito</option>
+</select>
+<input type="submit" />
+</form>
+<form action="." method="GET">
+<input type="submit" name="operacao" value="SALDO" />
+<input type="submit" name="operacao" value="EXTRATO" />
+</form>
 </body>
